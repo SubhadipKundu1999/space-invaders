a9761d9b34ed651636d0fa6d9f900ee8e5dc200e
Suhadip Kundu <subhadipkundu1000@gmail.com>
2024-02-08 08:50:58 +0530

    create projectiles

--- index.js
+++ index.js
@@ -17,23 +17,45 @@ let keys = {
     },
     d: {
         pressed: false
+    },
+    space: {
+        pressed: false
     }
 }
 
 let move = 1;
 
 
-//player class
+//player instance
 const player = new Player();
-console.log(player);
+
+//projectile 
+let projectiles = []
+
+
+
 
 
 function animate() {
     requestAnimationFrame(animate);
-    c.fillStyle = 'black';
 
+    c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
+
+    projectiles.forEach((projectile, index) => {
+
+        if (projectile.position.y + projectile.radius <= 0) {
+            projectiles.splice(index, 1)
+        }
+        else {
+            projectile.update();
+        }
+        //remove the projectile out of screen
+
+    })
+
+
     if (keys.a.pressed && player.position.x >= 0) {
         player.rotation = - 0.15;
         player.velocity.x = -5;
@@ -46,6 +68,7 @@ function animate() {
         player.velocity.x = 0;
         player.rotation = 0;
     }
+
 
 
 
@@ -65,6 +88,26 @@ addEventListener('keydown', ({ key }) => {
         case 'd':
             // player.velocity.x = +5;
             keys.d.pressed = true;
+            break;
+        case ' ':
+            console.log(key)
+            projectiles.push(
+                new Projectile(
+
+                    position = {
+                        x: player.position.x + player.width / 4,
+                        y: player.position.y,
+                    },
+                    velocity = {
+                        x: 0,
+                        y: -10
+                    },
+                    radius = 3
+
+                )
+            )
+            console.log(projectiles);
+            break
     }
 
 })
@@ -78,7 +121,11 @@ addEventListener('keyup', ({ key }) => {
             break;
         case 'd':
             keys.d.pressed = false;
+        case 'space':
+            keys.space.pressed = false
+            console.log(key)
     }
+
 
 })
 
